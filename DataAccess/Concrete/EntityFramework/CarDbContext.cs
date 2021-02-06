fb4d5fd0ab94e1a95e84be8241e4f90cc10fb4cd
Ali Alpay <alialpay96@hotmail.com>
2021-02-06 17:56:51 +0300

    06.02.2021

--- DataAccess/Concrete/EntityFramework/CarDbContext.cs
+++ DataAccess/Concrete/EntityFramework/CarDbContext.cs
@@ -14,8 +14,8 @@ namespace DataAccess.Concrete.EntityFramework
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CarDb;Trusted_Connection=true");
         }
 
-        public DbSet<Car> Car { get; set; }
-        public DbSet<Color> Color { get; set; }
-        public DbSet<Brand> Brand { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
     }
 }
