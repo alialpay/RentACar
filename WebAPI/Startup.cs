80ee48e37985f510ce5aa45736eae276b4a74e72
Ali Alpay <alialpay96@hotmail.com>
2021-03-10 23:40:52 +0300

    ders 15

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -49,7 +51,6 @@ namespace WebAPI
             //services.AddSingleton<IUserService, UserManager>();
             //services.AddSingleton<IUserDal, EfUserDal>();
 
-            services.AddSingleton<HttpContextAccessor, HttpContextAccessor>();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -67,7 +68,10 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-            ServiceTool.Create(services);
+
+            services.AddDependencyResolvers(new ICoreModule[] { 
+                new CoreModule()
+            });
 
         }
 
@@ -78,7 +82,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
+             
             app.UseHttpsRedirection();
 
             app.UseRouting();
